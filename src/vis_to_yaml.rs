40f7d3c69e12399c14b883b74b9960cda0297615
AL-Hareth <alharethturab@gmail.com>
2025-12-15 20:31:28 +0300

    FIX: made the functions work for general cases

--- src/vis_to_yaml.rs
+++ src/vis_to_yaml.rs
@@ -1,3 +1,5 @@
+use std::{fs, io::Write};
+
 /// This function handles the way we write custom rules and errors and converts them to valid yaml
 /// Example:
 ///     min_length: 5
@@ -15,25 +17,88 @@ pub fn handle_custom_rule_format(rules: String) -> String {
     let mut rule = String::new();
     let mut final_rules = String::new();
     while let Some(line) = rules.next() {
-        if line.starts_with("      error") {
+        if line.trim().starts_with("error") && !rule.trim().starts_with("value") {
             if rule.is_empty() {
                 // validates if the error is written without a rule before it
-                eprintln!("Error is put without a rule");
+                eprintln!("Err: No rules provided");
                 continue;
             }
-            let prev_rule = rule.split_once(':').unwrap();
+            let number_of_spaces = line.find("error").unwrap();
+            let (rule_name, rule_value) = rule.split_once(':').unwrap();
             let error_msg = &line.split_once(':').unwrap().1.trim().to_string();
+
             let result = format!(
-                "      {}: {{ \"value\": {}, \"error\": {} }}\n",
-                &prev_rule.0.trim(),
-                &prev_rule.1.trim(),
+                "{}{}: {{ \"value\": {}, \"error\": {} }}\n",
+                String::from(" ").repeat(number_of_spaces),
+                rule_name.trim(),
+                rule_value.trim(),
                 error_msg
             );
-            final_rules.push_str(&result);
-        }
 
+            let mut trimmed_end = final_rules.lines().collect::<Vec<&str>>();
+            trimmed_end.pop(); // this prevents the repetition of rules
+            trimmed_end.push(&result);
+            final_rules = trimmed_end.join("\n");
+        } else {
+            final_rules.push_str(line.as_str());
+            final_rules.push_str("\n");
+        }
         rule = line;
     }
 
     final_rules
+}
+
+/// This function is supposed to read the .vis input file, convert the custom formats into valid
+/// yaml and then write it to the output file in yaml extension
+pub fn read_vis_file(vis_path: &str) {
+    let vis_file = fs::read_to_string(vis_path).unwrap();
+    let mut output_file = fs::File::create("output.yaml").unwrap();
+    let mut lines = vis_file.lines();
+    let mut rules_batch = String::new();
+    let mut writing_rules = false;
+    let mut collecting_rules = false;
+    let mut rules_spaces = 0;
+    while let Some(mut line) = lines.next() {
+        if line.trim().starts_with("rules:") {
+            rules_spaces = line.find("rules:").unwrap();
+            collecting_rules = true;
+            output_file.write_all(line.as_bytes()).unwrap();
+            output_file.write_all(b"\n").unwrap();
+            continue;
+        }
+
+        while collecting_rules {
+            if !line.starts_with(format!("{} ", " ".repeat(rules_spaces)).as_str()) {
+                collecting_rules = false;
+                writing_rules = true;
+            }
+            rules_batch.push_str(line);
+            rules_batch.push_str("\n");
+            line = lines
+                .next()
+                .or_else(|| {
+                    collecting_rules = false;
+                    Some("EOF")
+                })
+                .unwrap();
+        }
+        if line == "EOF" {
+            break;
+        }
+
+        if writing_rules {
+            let rules = handle_custom_rule_format(rules_batch.clone());
+            output_file.write_all(rules.as_bytes()).unwrap();
+            rules_batch = String::new();
+            writing_rules = false;
+        }
+        output_file.write_all(line.as_bytes()).unwrap();
+        output_file.write_all(b"\n").unwrap();
+    }
+
+    if !rules_batch.is_empty() {
+        let rules = handle_custom_rule_format(rules_batch.clone());
+        output_file.write_all(rules.as_bytes()).unwrap();
+    }
 }
