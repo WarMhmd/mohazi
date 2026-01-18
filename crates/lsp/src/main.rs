73228f80760642011cb6836ccc77a77334b398d2
AL-Hareth <alharethturab@gmail.com>
2026-01-18 22:09:38 +0300

    changed core crate to core_lib

--- crates/lsp/src/main.rs
+++ crates/lsp/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod lsp;
+use lsp::Backend;
+use tower_lsp::{LspService, Server};
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
