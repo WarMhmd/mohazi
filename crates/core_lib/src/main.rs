857b4703689e86c03425bb90b81419cf29bcff98
Mohammad <mhmdsaalaa@gmail.com>
2026-02-12 21:02:36 +0300

    Fix a bug in main

--- crates/core_lib/src/main.rs
+++ crates/core_lib/src/main.rs
@@ -2,7 +2,7 @@ mod ast;
 mod vis_parser;
 
 fn main() {
-    let path = "/home/alhareth/projects/mohazi/crates/core_lib/src/tests/test_grad.vis";
+    let path = "./vis/test.vis";
     let file_content = std::fs::read_to_string(path).unwrap();
 
     match vis_parser::parse_vis(&file_content) {
